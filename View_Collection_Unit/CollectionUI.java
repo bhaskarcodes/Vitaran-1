42ea5a748570140e16847b7664469aff4daaa2ab
DebanjanaKar <debanjana.kar@gmail.com>
2017-04-27 11:52:43 +0530

    Collection Unit modules

---
+++ View_Collection_Unit/CollectionUI.java
@@ -0,0 +1,35 @@
+package com.example.jayanta.test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CollectionUI extends AppCompatActivity {
+
+    public Button notify;
+
+    public void init(){
+        notify=(Button)findViewById(R.id.button);
+        notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openUI= new Intent(CollectionUI.this, MapsActivity.class);
+                startActivity(openUI);
+            }
+        });
+
+    }
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection_ui);
+        init();
+    }
+}
